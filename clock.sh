1aaf8bbe06b33ab0455b17df661a90d0d3bca707
drlellinger <39723641+drlellinger@users.noreply.github.com>
2019-01-24 22:16:15 +0100

    Add files via upload

--- clock.sh
+++ clock.sh
@@ -2,23 +2,20 @@
 #Deklaration der Variablen:
 minute="$(date +"%M")"
 hour="$(date +"%I")"
-stunde="$(date +"%H")"
-tag="$(date +"%d")"
-monat="$(date +"%m")"
 	if [[ $minute == 30 ]]
 	then
-	  afplay ~/big-ben-macos/half-hour.mp3
+	  afplay /Users/maluck/big-ben-macos/half-hour.mp3
 	elif [[ $minute == 15 ]]
 	then
-	  afplay ~/big-ben-macos/quarter-hour.mp3
+	  afplay /Users/maluck/big-ben-macos/quarter-hour.mp3
 	elif [[ $minute == 45 ]]
 	then
-	  afplay ~/big-ben-macos/3quarter-hour.mp3
+	  afplay /Users/maluck/big-ben-macos/3quarter-hour.mp3
 	elif [[ $minute == 00 ]]
 	then
-	  afplay ~/big-ben-macos/full-hour.mp3
+	  afplay /Users/maluck/big-ben-macos/full-hour.mp3
 	  x=0
-	  while [ $x -lt $hour ]
+	  while [ $x -lt "$hour" ]
 	  do
 	    afplay ~/big-ben-macos/strike.mp3
 	    let x=x+1
